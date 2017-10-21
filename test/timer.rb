e7455ed256af0eee186997d186575635d841f0f7
Rick Hull <rick.hull@gmail.com>
2017-10-21 20:18:09 +0000

    move Timer tests from lib/ to test/

---
+++ test/timer.rb
@@ -0,0 +1,40 @@
+require 'compsci/timer'
+
+include CompSci
+
+start = Time.now
+t = Time.now
+puts
+print "running: "
+block_time = Timer.loop_average(count: 50) {
+  print '.'
+  sleep 0.01
+}
+puts
+puts "each: %0.3f" % block_time
+puts "elapsed: %0.3f" % (Time.now - t)
+puts "cumulative: %0.3f" % (Time.now - start)
+
+t = Time.now
+puts
+print "running: "
+block_time = Timer.loop_average(seconds: 0.5) {
+  print '.'
+  sleep 0.02
+}
+puts
+puts "each: %0.3f" % block_time
+puts "elapsed: %0.3f" % (Time.now - t)
+puts "cumulative: %0.3f" % (Time.now - start)
+
+t = Time.now
+puts
+print "running: "
+block_time = Timer.loop_average(seconds: 1) {
+  print '.'
+  sleep 2
+}
+puts
+puts "each: %0.3f" % block_time
+puts "elapsed: %0.3f" % (Time.now - t)
+puts "cumulative: %0.3f" % (Time.now - start)
