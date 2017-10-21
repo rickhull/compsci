2d1498f2122115b47dc465d9c32594f792032950
Rick Hull <rick.hull@gmail.com>
2017-10-21 10:35:37 +0000

    cleanup

--- heap.rb
+++ heap.rb
@@ -3,19 +3,19 @@
 #
 
 class Heap
-  def self.children_idx(idx)
-    [2 * idx + 1, 2 * idx + 2]
+  def self.parent_idx(idx)
+    (idx-1) / 2
   end
 
-  def self.parent_idx(idx)
-    (idx - 1) / 2
+  def self.children_idx(idx)
+    [2*idx + 1, 2*idx + 2]
   end
 
   def self.heapish?(parent, child, cmp_val)
     (parent <=> child) != (cmp_val * -1)
   end
 
-  attr_reader :storage
+  attr_reader :store
 
   def initialize(cmp_val: 1, minheap: false)
     cmp_val = -1 if minheap
@@ -25,11 +25,50 @@ class Heap
     else
       raise(ArgumentError, "unknown comparison value: #{cmp_val}")
     end
-    @storage = []
+    @store = []
   end
 
   def heapish_idx?(pidx, cidx)
-    self.class.heapish?(@storage[pidx], @storage[cidx], @cmp_val)
+    self.class.heapish?(@store[pidx], @store[cidx], @cmp_val)
+  end
+
+  def push(node)
+    @store << node
+    self.sift_up(self.last_idx)
+  end
+
+  def pop
+    node = @store.shift        # remove root node
+    @store.unshift(@store.pop) # move last node to root
+    self.sift_down(0)
+    node
+  end
+
+  def last_idx
+    @store.length - 1
+  end
+
+  def sift_up(idx)
+    return self if idx <= 0
+    pidx = self.class.parent_idx(idx)
+    if !self.heapish_idx?(pidx, idx)
+      @store[idx], @store[pidx] = @store[pidx], @store[idx] # swap
+      self.sift_up(pidx)
+    end
+    self
+  end
+
+  def sift_down(idx)
+    return self if idx > self.last_idx
+    lidx, ridx = self.class.children_idx(idx)
+    # take the child most likely to be a good parent
+    cidx = self.heapish_idx?(lidx, ridx) ? lidx : ridx
+
+    if !self.heapish_idx?(idx, cidx)
+      @store[idx], @store[cidx] = @store[cidx], @store[idx] # swap
+      self.sift_down(cidx)
+    end
+    self
   end
 
   def heap?(idx: 0)
@@ -43,76 +82,24 @@ class Heap
     check_children.each { |cidx| return false unless self.heap?(idx: cidx) }
     true
   end
-
-  # When you add a new node to a heap, you add it to the rightmost unoccupied
-  # leaf on the lowest level. Then you upheap that node until it has reached
-  # its proper position. In this way, the heap's order is maintained and the
-  # heap remains a complete tree.
-  def push(node)
-    @storage << node
-    self.sift_up(self.last_idx)
-  end
-
-  def pop
-    node = @storage.shift          # remove root node
-    @storage.unshift(@storage.pop) # move last node to root
-    self.sift_down(0)
-    node
-  end
-
-  def last_idx
-    @storage.length - 1
-  end
-
-  def sift_up(idx)
-    return self if idx <= 0
-    parent_idx = self.class.parent_idx(idx)
-    if !self.heapish_idx?(parent_idx, idx)
-      # swap
-      current = @storage[idx]
-      @storage[idx] = @storage[parent_idx]
-      @storage[parent_idx] = current
-      self.sift_up(parent_idx)
-    end
-    self
-  end
-
-  def sift_down(idx)
-    return self if idx >= @storage.length
-    current = @storage[idx]
-    lidx, ridx = self.class.children_idx(idx)
-    # take the child most likely to be a good parent
-    cidx = self.heapish_idx?(lidx, ridx) ? lidx : ridx
-
-    if !self.heapish_idx?(idx, cidx)
-      # swap
-      @storage[idx] = @storage[cidx]
-      @storage[cidx] = current
-      self.sift_down(cidx)
-    end
-    self
-  end
 end
 
 if __FILE__ == $0
   EYEBALL_TEST = true
-  INSERT_TEST = true
+  INSERT_TEST = false
   BENCHMARK_TEST = true
 
   if EYEBALL_TEST
     h = Heap.new
     99.times {
-      r = rand 99
-      puts "push #{r}"
-      h.push r
-      puts
+      h.push rand 99
     }
-    p h.storage
+    p h.store
     p h.heap?
 
     max = h.pop
     puts "popped #{max}"
-    p h.storage
+    p h.store
     p h.heap?
   end
 
