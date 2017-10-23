bf7d13ac4c43dc13be0b87a968563e01769e68bd
Rick Hull <rick.hull@gmail.com>
2017-10-23 20:18:02 +0000

    add the rest of the Fib benchmarks

--- test/bench/fib.rb
+++ test/bench/fib.rb
@@ -6,6 +6,14 @@ include CompSci
 
 # CLASSIC_RANGE = [3, 5, 10, 15, 20, 25, 30, 31, 32, 33, 34, 35]
 CLASSIC_RANGE = [10, 15, 20, 25, 30, 31, 32, 33, 34, 35]
+# RECURSIVE_RANGE = [10, 100, 1000, 2500, 5000, 7500]
+RECURSIVE_RANGE = [100, 1000, 2500, 5000, 7500]
+# CACHE_RANGE = [100, 1000, 10000, 100000, 112500, 125000]
+CACHE_RANGE = [100, 1000, 10000, 100000]
+
+# this causes churn at the process level and impacts other benchmarks
+# DYNAMIC_RANGE = [100, 1000, 10000, 100000, 200000, 500000]
+DYNAMIC_RANGE = [100, 1000, 10000, 100000]
 
 SPEC_BENCHMARK = true
 CLASS_BENCHMARK = true
@@ -13,7 +21,7 @@ BENCHMARK_IPS = true
 
 
 if SPEC_BENCHMARK
-  describe "Benchmark" do
+  describe "Fibonacci.classic Benchmark" do
     bench_range do
       CLASSIC_RANGE
     end
@@ -22,10 +30,38 @@ if SPEC_BENCHMARK
     bench_performance_exponential(*fc) do |n|
       Fibonacci.classic(n)
     end
+  end
+
+  describe "Fibonacci.cache_recursive Benchmark" do
+    bench_range do
+      RECURSIVE_RANGE
+    end
 
     fcr = ["Fibonacci.cache_recursive (linear, 0.95)", 0.95]
     bench_performance_linear(*fcr) do |n|
       Fibonacci.cache_recursive(n)
+    end
+  end
+
+  describe "Fibonacci.cache_iterative Benchmark" do
+    bench_range do
+      CACHE_RANGE
+    end
+
+    fci = ["Fibonacci.cache_iterative (linear, 0.99)", 0.99]
+    bench_performance_linear(*fci) do |n|
+      Fibonacci.cache_iterative(n)
+    end
+  end
+
+  describe "Fibonacci.dynamic Benchmark" do
+    bench_range do
+      DYNAMIC_RANGE
+    end
+
+    fd = ["Fibonacci.dynamic (linear, 0.99)", 0.99]
+    bench_performance_linear(*fd) do |n|
+      Fibonacci.dynamic(n)
     end
   end
 end
