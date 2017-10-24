1ac84e4830d9ab9cb1568b91d4ca909344df063b
Rick Hull <rick.hull@gmail.com>
2017-10-24 21:15:57 +0000

    factor out generic tree behavior
    - N child slots
    - BinaryTree is just a special case where N=2
    - only specific behavior is BinaryTree#bf_print

--- lib/compsci/binary_tree.rb
+++ lib/compsci/binary_tree.rb
@@ -1,17 +1,12 @@
 require 'compsci'
 
 module CompSci
-  class BinaryTree
-    attr_reader :root
+  class Tree
+    attr_reader :root, :child_slots
 
-    def initialize(root: nil, value: nil)
-      if root
-        @root = root
-      elsif value
-        @root = Node.new value
-      else
-        raise "a root node is required"
-      end
+    def initialize(root_node, child_slots: 2)
+      @root = root_node
+      @child_slots = child_slots
       @open_parent = @root
     end
 
@@ -19,14 +14,17 @@ module CompSci
       self.open_parent.new_child value
     end
 
+    def open_parent?(node)
+      node.children.size < @child_slots
+    end
+
     def open_parent
-      return @open_parent if @open_parent.open?
-      @open_parent = self.bf_search { |n| n.open? }
+      return @open_parent if self.open_parent?(@open_parent)
+      @open_parent = self.bf_search { |n| self.open_parent?(n) }
     end
 
     def df_search(node: nil, &blk)
       node ||= @root
-      puts "visited #{node}"
       return node if yield node
       node.children.each { |c|
         stop_node = self.df_search(node: c, &blk)
@@ -50,59 +48,30 @@ module CompSci
         return node if yield node
         destinations += node.children
       end
-    end
-
-    def bf_print(node: nil, width: 80)
-      count = 0
-      self.bf_search(node: node) { |n|
-        count += 1
-        str = n.to_s
-        level = Math.log(count, 2).floor
-        block_width = width / (2**level)
-        puts if 2**level == count and count > 1
-        print n.to_s.ljust(block_width / 2, ' ').rjust(block_width, ' ')
-      }
-      puts
+      nil
     end
 
     class Node
       attr_accessor :value, :parent
-      attr_reader :left, :right
+      attr_reader :children
 
       def initialize(value)
         @value = value
         @parent = nil
-        @left = nil
-        @right = nil
+        @children = []
         # @metadata = {}
       end
 
       def add_child(node)
-        if @left.nil?
-          @left = node
-          @left.parent = self
-        elsif @right.nil?
-          @right = node
-          @right.parent = self
-        else
-          raise("no child slot available for #{node}")
-        end
+        @children << node
       end
 
       def new_child(value)
-        self.add_child self.class.new value
+        self.add_child self.class.new(value)
       end
 
       def add_parent(node)
         node.add_child(self)
-      end
-
-      def open?
-        @left.nil? or @right.nil?
-      end
-
-      def children
-        [@left, @right].compact
       end
 
       def to_s
@@ -110,13 +79,32 @@ module CompSci
       end
 
       def inspect
-        "#<Node: %s @left=%s @right=%s>" % [@value.to_s,
-                                            @left.inspect,
-                                            @right.inspect]
+        "#<Node: %s @children=[%s]>" % [self.to_s, @children.to_s]
       end
     end
   end
 
+  class BinaryTree < Tree
+    def initialize(root_node)
+      super(root_node, child_slots: 2)
+    end
+
+    def bf_print(node: nil, width: 80)
+      count = 0
+      self.bf_search(node: node) { |n|
+        count += 1
+        level = Math.log(count, 2).floor
+        block_width = width / (2**level)
+        puts if 2**level == count and count > 1
+        print n.to_s.ljust(block_width / 2, ' ').rjust(block_width, ' ')
+      }
+      puts
+    end
+  end
+
+  # A CompleteBinaryTree can very efficiently use an array for storage using
+  # simple arithmetic to determine parent child relationships.
+  #
   class CompleteBinaryTree
     # integer math says idx 2 and idx 1 both have parent at idx 0
     def self.parent_idx(idx)
