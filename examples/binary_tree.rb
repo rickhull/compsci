3802e3c359844402001c6e80989307fa1fdca26e
Rick Hull <rick.hull@gmail.com>
2017-10-24 21:22:43 +0000

    add examples/binary_tree.rb

---
+++ examples/binary_tree.rb
@@ -0,0 +1,18 @@
+require 'compsci/tree'
+
+include CompSci
+
+vals = []
+30.times { vals << rand(99) }
+p vals
+
+root_node = Tree::Node.new vals.shift
+tree = BinaryTree.new(root_node)
+tree.push vals.shift until vals.empty?
+
+tree.bf_print
+
+tree.df_search { |n|
+  puts "visited #{n}"
+  false # or n.value > 90
+}
