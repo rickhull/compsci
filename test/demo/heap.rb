1091c0116ee060b8857e7cbf7209b5e322714f0e
Rick Hull <rick.hull@gmail.com>
2017-10-24 05:44:49 +0000

    tweak heap demo

--- test/demo/heap.rb
+++ test/demo/heap.rb
@@ -3,6 +3,45 @@ require 'compsci/heap'
 include CompSci
 
 #
+# 3 seconds worth of inserts
+#
+
+count = 0
+t = Time.now
+t2 = Time.now
+h = Heap.new
+elapsed = 0
+
+while elapsed < 3
+  t1 = Time.now
+  h.push rand 99999
+  e1 = Time.now - t1
+  elapsed = Time.now - t
+  count += 1
+  puts "%ith push: %0.8f s" % [count, e1] if count % 10000 == 0
+
+  if count % 100000 == 0
+    e2 = Time.now - t2
+    t2 = Time.now
+    puts "-------------"
+    puts "    100k push: %0.8f s (%ik push / s)" % [e2, 100.to_f / e2]
+    puts
+  end
+end
+
+puts "pushed %i items in %0.1f s" % [count, elapsed]
+puts
+
+print "still a heap with #{h.size} items? "
+t = Time.now
+if h.heap?
+  puts "YES - %0.2f sec" % (Time.now - t)
+else
+  puts "NO - %0.4f sec" % (Time.now - t)
+end
+puts
+
+#
 # 99 inserts; display the internal array
 #
 
@@ -18,7 +57,7 @@ puts "heap store: #{h.store.inspect}"
 puts "heap: #{h.heap?}"
 puts
 
-print "pop: %i" % h.pop
+puts "pop: %i" % h.pop
 puts "heap store: #{h.store.inspect}"
 puts "heap: #{h.heap?}"
 puts
@@ -31,23 +70,3 @@ puts "]"
 puts "heap store: #{h.store.inspect}"
 puts "heap: #{h.heap?}"
 puts
-
-#
-# 3 seconds worth of inserts
-#
-
-count = 1
-t = Time.now
-h = Heap.new
-elapsed = 0
-
-while elapsed < 3
-  t1 = Time.now
-  h.push rand 99999
-  e1 = Time.now - t1
-  elapsed = Time.now - t
-  count += 1
-  puts "%ith push: %0.8f s" % [count, e1] if count % 10000 == 0
-end
-
-puts "pushed %i items in %0.1f s" % [count, elapsed]
