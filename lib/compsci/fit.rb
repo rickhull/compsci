4420591a4c554f34119807c8aece8291d1f24f68
Rick Hull <rick.hull@gmail.com>
2017-10-25 22:23:18 +0000

    move fit stuff into its own file and module

---
+++ lib/compsci/fit.rb
@@ -0,0 +1,137 @@
+require 'compsci'
+
+module CompSci::Fit
+  #
+  # functions below originally copied from https://github.com/seattlrb/minitest
+  #
+
+  ##
+  # Enumerates over +enum+ mapping +block+ if given, returning the
+  # sum of the result. Eg:
+  #
+  #   sigma([1, 2, 3])                # => 1 + 2 + 3 => 7
+  #   sigma([1, 2, 3]) { |n| n ** 2 } # => 1 + 4 + 9 => 14
+
+  def self.sigma enum, &block
+    enum = enum.map(&block) if block
+    enum.inject { |sum, n| sum + n }
+  end
+
+  ##
+  # Takes an array of x/y pairs and calculates the general R^2 value to
+  # measure fit against a predictive function,  which is the block supplied
+  # to error:
+  #
+  # e.g. error(xys) { |x| 5 + 2 * x }
+  #
+  # See: http://en.wikipedia.org/wiki/Coefficient_of_determination
+  #
+
+  def self.error xys, &blk
+    y_bar  = sigma(xys) { |_, y| y                   } / xys.size.to_f
+    ss_tot = sigma(xys) { |_, y| (y - y_bar)    ** 2 }
+    ss_res = sigma(xys) { |x, y| (yield(x) - y) ** 2 }
+
+    1 - (ss_res / ss_tot)
+  end
+
+  ##
+  # Fits the functional form: a
+  #
+  # Takes x and y values and returns [a, variance]
+  #
+
+  def self.constant xs, ys
+    # written by Rick
+    y_bar = sigma(ys) / ys.size.to_f
+    variance = sigma(ys) { |y| (y - y_bar) ** 2 }
+    [y_bar, variance]
+  end
+
+  ##
+  # To fit a functional form: y = a + b*ln(x).
+  #
+  # Takes x and y values and returns [a, b, r^2].
+  #
+  # See: http://mathworld.wolfram.com/LeastSquaresFittingLogarithmic.html
+
+  def self.logarithmic xs, ys
+    n     = xs.size
+    xys   = xs.zip(ys)
+    slnx2 = sigma(xys) { |x, _| Math.log(x) ** 2 }
+    slnx  = sigma(xys) { |x, _| Math.log(x)      }
+    sylnx = sigma(xys) { |x, y| y * Math.log(x)  }
+    sy    = sigma(xys) { |_, y| y                }
+
+    c = n * slnx2 - slnx ** 2
+    b = ( n * sylnx - sy * slnx ) / c
+    a = (sy - b * slnx) / n
+
+    return a, b, self.error(xys) { |x| a + b * Math.log(x) }
+  end
+
+  ##
+  # Fits the functional form: a + bx.
+  #
+  # Takes x and y values and returns [a, b, r^2].
+  #
+  # See: http://mathworld.wolfram.com/LeastSquaresFitting.html
+
+  def self.linear xs, ys
+    n   = xs.size
+    xys = xs.zip(ys)
+    sx  = sigma xs
+    sy  = sigma ys
+    sx2 = sigma(xs)  { |x|   x ** 2 }
+    sxy = sigma(xys) { |x, y| x * y  }
+
+    c = n * sx2 - sx**2
+    a = (sy * sx2 - sx * sxy) / c
+    b = ( n * sxy - sx * sy ) / c
+
+    return a, b, self.error(xys) { |x| a + b * x }
+  end
+
+  ##
+  # To fit a functional form: y = ae^(bx).
+  #
+  # Takes x and y values and returns [a, b, r^2].
+  #
+  # See: http://mathworld.wolfram.com/LeastSquaresFittingExponential.html
+
+  def self.exponential xs, ys
+    n     = xs.size
+    xys   = xs.zip(ys)
+    sxlny = sigma(xys) { |x, y| x * Math.log(y) }
+    slny  = sigma(xys) { |_, y| Math.log(y)     }
+    sx2   = sigma(xys) { |x, _| x * x           }
+    sx    = sigma xs
+
+    c = n * sx2 - sx ** 2
+    a = (slny * sx2 - sx * sxlny) / c
+    b = ( n * sxlny - sx * slny ) / c
+
+    return Math.exp(a), b, self.error(xys) { |x| Math.exp(a + b * x) }
+  end
+
+  ##
+  # To fit a functional form: y = ax^b.
+  #
+  # Takes x and y values and returns [a, b, r^2].
+  #
+  # See: http://mathworld.wolfram.com/LeastSquaresFittingPowerLaw.html
+
+  def self.power xs, ys
+    n       = xs.size
+    xys     = xs.zip(ys)
+    slnxlny = sigma(xys) { |x, y| Math.log(x) * Math.log(y) }
+    slnx    = sigma(xs)  { |x   | Math.log(x)               }
+    slny    = sigma(ys)  { |   y| Math.log(y)               }
+    slnx2   = sigma(xs)  { |x   | Math.log(x) ** 2          }
+
+    b = (n * slnxlny - slnx * slny) / (n * slnx2 - slnx ** 2)
+    a = (slny - b * slnx) / n
+
+    return Math.exp(a), b, self.error(xys) { |x| (Math.exp(a) * (x ** b)) }
+  end
+end
--- test/fit.rb
+++ test/fit.rb
@@ -1,19 +1,21 @@
-require 'compsci'
+require 'compsci/fit'
 require 'minitest/autorun'
 
-describe CompSci do
+include CompSci
+
+describe Fit do
   before do
     @xs = [1, 2, 5, 10, 20, 50, 100, 200, 500]
   end
 
   it "must sigma" do
-    CompSci.sigma([1, 2, 3]).must_equal 6
-    CompSci.sigma([1, 2, 3]) { |n| n ** 2 }.must_equal 14
+    Fit.sigma([1, 2, 3]).must_equal 6
+    Fit.sigma([1, 2, 3]) { |n| n ** 2 }.must_equal 14
   end
 
   it "must fit_error" do
-    CompSci.fit_error([[1, 1], [2, 2], [3, 3]]) { |x| x }.must_equal 1.0
-    CompSci.fit_error([[1, 1], [2, 2], [3, 4]]) { |x| x }.must_be :<, 0.8
+    Fit.error([[1, 1], [2, 2], [3, 3]]) { |x| x }.must_equal 1.0
+    Fit.error([[1, 1], [2, 2], [3, 4]]) { |x| x }.must_be :<, 0.8
   end
 
   # y = a
@@ -22,7 +24,7 @@ describe CompSci do
     as.each { |a|
       # note, this test can possibly fail depending on what rand spits out
       ys = @xs.map { |x| a + (rand - 0.5) }
-      y_bar, variance = CompSci.fit_constant(@xs, ys)
+      y_bar, variance = Fit.constant(@xs, ys)
       var_val = variance / ys.size
       y_bar.must_be_close_to a, 0.3
       var_val.must_be_close_to 0.1, 0.09
@@ -35,7 +37,7 @@ describe CompSci do
     bs = [-9999, -2000, -500, -0.01, 0.01, 500, 2000, 9999]
     as.each { |a|
       bs.each { |b|
-        ary = CompSci.fit_logarithmic(@xs, @xs.map { |x| a + b * Math.log(x) })
+        ary = Fit.logarithmic(@xs, @xs.map { |x| a + b * Math.log(x) })
         ary[0].must_be_close_to a
         ary[1].must_be_close_to b
         ary[2].must_equal 1.0
@@ -49,7 +51,7 @@ describe CompSci do
     bs = [-9999, -2000, -500, -0.01, 0.01, 500, 2000, 9999]
     as.each { |a|
       bs.each { |b|
-        ary = CompSci.fit_linear(@xs, @xs.map { |x| a + b * x })
+        ary = Fit.linear(@xs, @xs.map { |x| a + b * x })
         ary[0].must_be_close_to a
         ary[1].must_be_close_to b
         ary[2].must_equal 1.0
@@ -82,7 +84,7 @@ describe CompSci do
     a = -50
     b = 1.3
     ys = @xs.map { |x| a + b * x**2 + x**3 }
-    ary = CompSci.fit_linear(@xs, ys)
+    ary = Fit.linear(@xs, ys)
     # ary[2].must_be :<, 0.8
     ary[2].must_be :<, 0.9
     if ary[2] > 0.85
@@ -106,7 +108,7 @@ describe CompSci do
     bs = [-1.4, -1.1, -0.1, 0.01, 0.5, 0.75]
     as.each { |a|
       bs.each { |b|
-        ary = CompSci.fit_exponential(@xs,
+        ary = Fit.exponential(@xs,
                                       @xs.map { |x| a * Math::E**(b * x) })
         ary[0].must_be_close_to a
         ary[1].must_be_close_to b
@@ -119,11 +121,11 @@ describe CompSci do
   it "must fit_power" do
     as = [0.01, 7.5, 500, 1000, 5000, 9999]
     bs = [-114, -100, -10, -0.5, -0.1, 0.1, 0.75, 10, 50, 60]
-    # -114 causes CompSci.fit_error warning: Bignum out of Float range
+    # -114 causes Fit.error warning: Bignum out of Float range
     bs.shift
     as.each { |a|
       bs.each { |b|
-        ary = CompSci.fit_power(@xs, @xs.map { |x| a * x**b })
+        ary = Fit.power(@xs, @xs.map { |x| a * x**b })
         ary[0].must_be_close_to a
         ary[1].must_be_close_to b
         ary[2].must_equal 1.0
