96e5ac0248484455eda345344f92ab8b0c977efe
Rick Hull <rick.hull@gmail.com>
2017-10-25 00:55:08 +0000

    move Heap.idx methods to test/tree; add CBT instance tests

--- test/heap.rb
+++ test/heap.rb
@@ -4,62 +4,6 @@ require 'minitest/autorun'
 include CompSci
 
 describe Heap do
-  it "must calculate a parent index" do
-    valid = {
-      1 => 0,
-      2 => 0,
-      3 => 1,
-      4 => 1,
-      5 => 2,
-      6 => 2,
-      7 => 3,
-      8 => 3,
-      9 => 4,
-      10 => 4,
-    }
-
-    invalid = {
-      0 => -1,
-      -1 => -1,
-      -2 => -2,
-    }
-    valid.each { |idx, pidx|
-      Heap.parent_idx(idx).must_equal pidx
-    }
-    invalid.each { |idx, pidx|
-      Heap.parent_idx(idx).must_equal pidx
-    }
-  end
-
-  it "must calculate children indices" do
-    valid = {
-      0 => [1, 2],
-      1 => [3, 4],
-      2 => [5, 6],
-      3 => [7, 8],
-      4 => [9, 10],
-      5 => [11, 12],
-      6 => [13, 14],
-      7 => [15, 16],
-      8 => [17, 18],
-      9 => [19, 20],
-      10 => [21, 22],
-    }
-
-    invalid = {
-      -3 => [-5, -4],
-      -2 => [-3, -2],
-      -1 => [-1, 0],
-    }
-
-    valid.each { |idx, cidx|
-      Heap.children_idx(idx).must_equal cidx
-    }
-    invalid.each { |idx, cidx|
-      Heap.children_idx(idx).must_equal cidx
-    }
-  end
-
   describe "instance" do
     before do
       @maxheap = Heap.new
