e30cd72aed1075cb08c409bb21961a90368a5c9e
Rick Hull <rick.hull@gmail.com>
2017-10-25 07:53:13 +0000

    cleanup

--- test/fib.rb
+++ test/fib.rb
@@ -4,12 +4,10 @@ require 'minitest/autorun'
 include CompSci
 
 describe Fibonacci do
-  describe "all methods" do
-    it "must calculate fib(10) == 55" do
-      Fibonacci.classic(10).must_equal 55
-      Fibonacci.cache_recursive(10).must_equal 55
-      Fibonacci.cache_iterative(10).must_equal 55
-      Fibonacci.dynamic(10).must_equal 55
-    end
+  it "must calculate fib(10) == 55" do
+    Fibonacci.classic(10).must_equal 55
+    Fibonacci.cache_recursive(10).must_equal 55
+    Fibonacci.cache_iterative(10).must_equal 55
+    Fibonacci.dynamic(10).must_equal 55
   end
 end
--- test/heap.rb
+++ test/heap.rb
@@ -4,45 +4,43 @@ require 'minitest/autorun'
 include CompSci
 
 describe Heap do
-  describe "instance" do
-    before do
-      @maxheap = Heap.new
-      @minheap = Heap.new(minheap: true)
-      @inserts = (1..10).to_a
-      @inserts.each { |i|
-        @maxheap.push i
-        @minheap.push i
-      }
-    end
+  before do
+    @maxheap = Heap.new
+    @minheap = Heap.new(minheap: true)
+    @inserts = (1..10).to_a
+    @inserts.each { |i|
+      @maxheap.push i
+      @minheap.push i
+    }
+  end
 
-    it "must satisfy the heap property" do
-      @maxheap.heap?.must_equal true
-      @minheap.heap?.must_equal true
-      @minheap.store.must_equal @inserts
-      @maxheap.store.wont_equal @inserts
-      @maxheap.store.wont_equal @inserts.reverse
-    end
+  it "must satisfy the heap property" do
+    @maxheap.heap?.must_equal true
+    @minheap.heap?.must_equal true
+    @minheap.store.must_equal @inserts
+    @maxheap.store.wont_equal @inserts
+    @maxheap.store.wont_equal @inserts.reverse
+  end
 
-    it "must recognize heap violations" do
-      @minheap.store.push 0
-      @minheap.heap?.must_equal false
-      @minheap.sift_up @minheap.last_idx
-      @minheap.heap?.must_equal true
+  it "must recognize heap violations" do
+    @minheap.store.push 0
+    @minheap.heap?.must_equal false
+    @minheap.sift_up @minheap.last_idx
+    @minheap.heap?.must_equal true
 
-      @minheap.store.unshift 10
-      @minheap.heap?.must_equal false
-      @minheap.sift_down 0
-      @minheap.heap?.must_equal true
+    @minheap.store.unshift 10
+    @minheap.heap?.must_equal false
+    @minheap.sift_down 0
+    @minheap.heap?.must_equal true
 
-      @maxheap.store.push 10
-      @maxheap.heap?.must_equal false
-      @maxheap.sift_up @maxheap.last_idx
-      @maxheap.heap?.must_equal true
+    @maxheap.store.push 10
+    @maxheap.heap?.must_equal false
+    @maxheap.sift_up @maxheap.last_idx
+    @maxheap.heap?.must_equal true
 
-      @maxheap.store.unshift 0
-      @maxheap.heap?.must_equal false
-      @maxheap.sift_down 0
-      @maxheap.heap?.must_equal true
-    end
+    @maxheap.store.unshift 0
+    @maxheap.heap?.must_equal false
+    @maxheap.sift_down 0
+    @maxheap.heap?.must_equal true
   end
 end
