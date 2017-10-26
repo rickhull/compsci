5ea567abd693b3377560e9215104f8947f8021ad
Rick Hull <rick.hull@gmail.com>
2017-10-26 02:35:52 +0000

    clean up examples/timer using Timer.since

--- examples/timer.rb
+++ examples/timer.rb
@@ -2,39 +2,41 @@ require 'compsci/timer'
 
 include CompSci
 
+overall_start = Timer.now
+
 start = Timer.now
-t = Timer.now
-puts
 print "running sleep 0.01 (50x): "
-block_time = Timer.loop_average(count: 50) {
+_answer, each_et = Timer.loop_average(count: 50) {
   print '.'
   sleep 0.01
 }
 puts
-puts "each: %0.3f" % block_time
-puts "elapsed: %0.3f" % (Timer.now - t)
-puts "cumulative: %0.3f" % (Timer.now - start)
-
-t = Timer.now
+puts "each: %0.3f" % each_et
+puts "elapsed: %0.3f" % Timer.since(start)
+puts "cumulative: %0.3f" % Timer.since(overall_start)
 puts
+
+
+start = Timer.now
 print "running sleep 0.02 (0.5 s): "
-block_time = Timer.loop_average(seconds: 0.5) {
+_answer, each_et = Timer.loop_average(seconds: 0.5) {
   print '.'
   sleep 0.02
 }
 puts
-puts "each: %0.3f" % block_time
-puts "elapsed: %0.3f" % (Timer.now - t)
-puts "cumulative: %0.3f" % (Timer.now - start)
-
-t = Timer.now
+puts "each: %0.3f" % each_et
+puts "elapsed: %0.3f" % Timer.since(start)
+puts "cumulative: %0.3f" % Timer.since(overall_start)
 puts
+
+
+start = Timer.now
 print "running sleep 2 (1 s): "
-block_time = Timer.loop_average(seconds: 1) {
+_answer, each_et = Timer.loop_average(seconds: 1) {
   print '.'
   sleep 2
 }
 puts
-puts "each: %0.3f" % block_time
-puts "elapsed: %0.3f" % (Timer.now - t)
-puts "cumulative: %0.3f" % (Timer.now - start)
+puts "each: %0.3f" % each_et
+puts "elapsed: %0.3f" % Timer.since(start)
+puts "cumulative: %0.3f" % Timer.since(overall_start)
