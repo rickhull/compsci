37a69cc08d6254fb18bf41500cc7d6cb0426b5a0
Rick Hull <rick.hull@gmail.com>
2017-10-26 06:23:15 +0000

    add 3 seconds of push like Heap example

--- examples/binary_tree.rb
+++ examples/binary_tree.rb
@@ -1,6 +1,50 @@
 require 'compsci/tree'
+require 'compsci/timer'
 
 include CompSci
+
+puts <<EOF
+#
+# 3 seconds worth of pushes
+#
+
+EOF
+
+count = 0
+start = Timer.now
+start_1k = Timer.now
+tree = BinaryTree.new(Tree::Node.new rand 99)
+
+loop {
+  count += 1
+
+  if count % 100 == 0
+    _ans, push_elapsed = Timer.elapsed { tree.push rand 99 }
+    puts "%ith push: %0.8f s" % [count, push_elapsed]
+  else
+    tree.push rand 99
+  end
+
+  if count % 1000 == 0
+    push_1k_elapsed = Timer.since start_1k
+    puts "-----------"
+    puts "    1k push: %0.4f s (%i push / s)" %
+         [push_1k_elapsed, 1000.to_f / push_1k_elapsed]
+    puts
+    start_1k = Timer.now
+  end
+  break if Timer.since(start) > 3
+}
+
+puts "pushed %i items in %0.1f s" % [count, Timer.since(start)]
+puts
+
+puts <<EOF
+#
+# 30 inserts, bf_print, df_search
+#
+
+EOF
 
 vals = []
 30.times { vals << rand(99) }
@@ -19,3 +63,4 @@ tree.df_search { |n|
 puts
 
 p tree
+puts
