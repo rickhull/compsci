335ce0b0f9f1a559b8fbbc305aecb4093fcc7c33
Rick Hull <rick.hull@gmail.com>
2017-10-27 07:06:56 +0000

    stop requiring 'compsci' (for now)

--- lib/compsci/fibonacci.rb
+++ lib/compsci/fibonacci.rb
@@ -1,39 +1,40 @@
-require 'compsci'
 autoload :Matrix, 'matrix'
 
-module CompSci::Fibonacci
-  def self.classic(n)
-    n < 2 ? n : classic(n-1) + classic(n-2)
-  end
+module CompSci
+  class Fibonacci
+    def self.classic(n)
+      n < 2 ? n : classic(n-1) + classic(n-2)
+    end
 
-  def self.cache_recursive(n, cache = {})
-    return n if n < 2
-    cache[n] ||= cache_recursive(n-1, cache) + cache_recursive(n-2, cache)
-  end
+    def self.cache_recursive(n, cache = {})
+      return n if n < 2
+      cache[n] ||= cache_recursive(n-1, cache) + cache_recursive(n-2, cache)
+    end
 
-  def self.cache_iterative(n)
-    cache = [0, 1]
-    2.upto(n) { |i| cache[i] = cache[i-1] + cache[i-2] }
-    cache[n]
-  end
+    def self.cache_iterative(n)
+      cache = [0, 1]
+      2.upto(n) { |i| cache[i] = cache[i-1] + cache[i-2] }
+      cache[n]
+    end
 
-  # traditional
-  def self.dynamic(n)
-    a, b = 0, 1
-    n.times { a, b = b, a+b }
-    a
-  end
+    # traditional
+    def self.dynamic(n)
+      a, b = 0, 1
+      n.times { a, b = b, a+b }
+      a
+    end
 
-  # fails for n == 0
-  def self.dynamic_fast(n)
-    a, b = 0, 1
-    (n-1).times { a, b = b, a+b }
-    b
-  end
+    # fails for n == 0
+    def self.dynamic_fast(n)
+      a, b = 0, 1
+      (n-1).times { a, b = b, a+b }
+      b
+    end
 
-  # https://gist.github.com/havenwood/02cf291b809327d96a3f
-  # slower than dynamic until around n == 500
-  def self.matrix(n)
-    (Matrix[[0, 1], [1, 1]] ** n.pred)[1, 1].to_i
+    # https://gist.github.com/havenwood/02cf291b809327d96a3f
+    # slower than dynamic until around n == 500
+    def self.matrix(n)
+      (Matrix[[0, 1], [1, 1]] ** n.pred)[1, 1].to_i
+    end
   end
 end
--- lib/compsci/timer.rb
+++ lib/compsci/timer.rb
@@ -1,36 +1,37 @@
-require 'compsci'
+module CompSci
 
-module CompSci::Timer
-  # lifted from seattlerb/minitest
-  if defined? Process::CLOCK_MONOTONIC
-    def self.now
-      Process.clock_gettime Process::CLOCK_MONOTONIC
+  module Timer
+    # lifted from seattlerb/minitest
+    if defined? Process::CLOCK_MONOTONIC
+      def self.now
+        Process.clock_gettime Process::CLOCK_MONOTONIC
+      end
+    else
+      def self.now
+        Time.now
+      end
     end
-  else
-    def self.now
-      Time.now
+
+    def self.since(t)
+      self.now - t
     end
-  end
 
-  def self.since(t)
-    self.now - t
-  end
+    def self.elapsed(&work)
+      t = self.now
+      return yield, self.since(t)
+    end
 
-  def self.elapsed(&work)
-    t = self.now
-    return yield, self.since(t)
-  end
-
-  def self.loop_avg(count: 999, seconds: 1, &work)
-    i = 0
-    start = self.now
-    val = nil
-    loop {
-      val = yield
-      i += 1
-      break if i >= count
-      break if self.since(start) > seconds
-    }
-    return val, self.since(start) / i.to_f
+    def self.loop_avg(count: 999, seconds: 1, &work)
+      i = 0
+      start = self.now
+      val = nil
+      loop {
+        val = yield
+        i += 1
+        break if i >= count
+        break if self.since(start) > seconds
+      }
+      return val, self.since(start) / i.to_f
+    end
   end
 end
