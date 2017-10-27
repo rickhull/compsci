f3371151bda6d9af81af6232d27d2fa03dde7f1c
Rick Hull <rick.hull@gmail.com>
2017-10-27 04:42:31 +0000

    split out complete_tree and node tests from tree.rb

---
+++ test/complete_tree.rb
@@ -0,0 +1,149 @@
+require 'compsci/complete_tree'
+require 'minitest/autorun'
+
+include CompSci
+
+describe CompleteBinaryTree do
+  it "must calculate a parent index" do
+    valid = {
+      1 => 0,
+      2 => 0,
+      3 => 1,
+      4 => 1,
+      5 => 2,
+      6 => 2,
+      7 => 3,
+      8 => 3,
+      9 => 4,
+      10 => 4,
+    }
+
+    invalid = {
+      0 => -1,
+      -1 => -1,
+      -2 => -2,
+    }
+    valid.each { |idx, pidx|
+      CompleteBinaryTree.parent_idx(idx).must_equal pidx
+    }
+    invalid.each { |idx, pidx|
+      CompleteBinaryTree.parent_idx(idx).must_equal pidx
+    }
+  end
+
+  it "must calculate children indices" do
+    valid = {
+      0 => [1, 2],
+      1 => [3, 4],
+      2 => [5, 6],
+      3 => [7, 8],
+      4 => [9, 10],
+      5 => [11, 12],
+      6 => [13, 14],
+      7 => [15, 16],
+      8 => [17, 18],
+      9 => [19, 20],
+      10 => [21, 22],
+    }
+
+    invalid = {
+      -3 => [-5, -4],
+      -2 => [-3, -2],
+      -1 => [-1, 0],
+    }
+
+    valid.each { |idx, cidx|
+      CompleteBinaryTree.children_idx(idx).must_equal cidx
+    }
+    invalid.each { |idx, cidx|
+      CompleteBinaryTree.children_idx(idx).must_equal cidx
+    }
+  end
+
+  describe "instance" do
+    before do
+      @array = (0..99).sort_by { rand }
+      @empty = CompleteBinaryTree.new
+      @nonempty = CompleteBinaryTree.new(store: @array)
+    end
+
+    it "must have a size" do
+      @empty.size.must_equal 0
+      @nonempty.size.must_equal @array.size
+    end
+
+    it "must have a last_idx, nil when empty" do
+      @empty.last_idx.nil?.must_equal true
+      @nonempty.last_idx.must_equal 99
+    end
+  end
+end
+
+describe CompleteNaryTree do
+  it "must calculate a parent index for N=3" do
+    valid = {
+      1 => 0,
+      2 => 0,
+      3 => 0,
+      4 => 1,
+      5 => 1,
+      6 => 1,
+      7 => 2,
+      8 => 2,
+      9 => 2,
+      10 => 3,
+    }
+
+    invalid = {
+      0 => -1,
+      -1 => -1,
+      -2 => -1,
+    }
+    valid.each { |idx, pidx|
+      CompleteNaryTree.parent_idx(idx, 3).must_equal pidx
+    }
+    invalid.each { |idx, pidx|
+      CompleteNaryTree.parent_idx(idx, 3).must_equal pidx
+    }
+  end
+
+  it "must calculate children indices for N=3" do
+    valid = {
+      0 => [1, 2, 3],
+      1 => [4, 5, 6],
+      2 => [7, 8, 9],
+      3 => [10, 11, 12],
+    }
+
+    invalid = {
+      -3 => [-8, -7, -6],
+      -2 => [-5, -4, -3],
+      -1 => [-2, -1,  0],
+    }
+
+    valid.each { |idx, cidx|
+      CompleteNaryTree.children_idx(idx, 3).must_equal cidx
+    }
+    invalid.each { |idx, cidx|
+      CompleteNaryTree.children_idx(idx, 3).must_equal cidx
+    }
+  end
+
+  describe "instance" do
+    before do
+      @array = (0..99).sort_by { rand }
+      @empty = CompleteNaryTree.new(child_slots: 5)
+      @nonempty = CompleteNaryTree.new(store: @array, child_slots: 3)
+    end
+
+    it "must have a size" do
+      @empty.size.must_equal 0
+      @nonempty.size.must_equal @array.size
+    end
+
+    it "must have a last_idx, nil when empty" do
+      @empty.last_idx.nil?.must_equal true
+      @nonempty.last_idx.must_equal 99
+    end
+  end
+end
---
+++ test/node.rb
@@ -0,0 +1,67 @@
+require 'compsci/node'
+require 'minitest/autorun'
+
+include CompSci
+
+describe Node do
+  before do
+    @martin_sheen = Node.new 'martin'
+    @charlie_sheen = Node.new 'charlie'
+    @emilio_estevez = Node.new 'emilio'
+  end
+
+  it "must start with no children" do
+    [@martin_sheen, @charlie_sheen, @emilio_estevez].each { |n|
+      n.children.must_be_empty
+    }
+  end
+
+  it "must track children" do
+    @charlie_sheen.add_parent(@martin_sheen)
+    @martin_sheen.children.must_include @charlie_sheen
+
+    @martin_sheen.children.wont_include @emilio_estevez
+    @martin_sheen.add_child @emilio_estevez
+    @martin_sheen.children.must_include @emilio_estevez
+  end
+
+  it "must create children from scalars" do
+    @martin_sheen.new_child 'fake_emilio'
+    @martin_sheen.children.size.must_equal 1
+    @martin_sheen.children.first.value.must_equal 'fake_emilio'
+    @martin_sheen.children.wont_include @emilio_estevez
+  end
+end
+
+describe ChildNode do
+  before do
+    @martin_sheen = ChildNode.new 'martin'
+    @charlie_sheen = ChildNode.new 'charlie'
+    @emilio_estevez = ChildNode.new 'emilio'
+  end
+
+  it "must start with neither parent nor children" do
+    [@martin_sheen, @charlie_sheen, @emilio_estevez].each { |n|
+      n.parent.nil?.must_equal true
+      n.children.must_be_empty
+    }
+  end
+
+  it "must track parent and children" do
+    @charlie_sheen.add_parent(@martin_sheen)
+    @charlie_sheen.parent.must_equal @martin_sheen
+    @martin_sheen.children.must_include @charlie_sheen
+
+    @martin_sheen.children.wont_include @emilio_estevez
+    @martin_sheen.add_child @emilio_estevez
+    @martin_sheen.children.must_include @emilio_estevez
+    @emilio_estevez.parent.must_equal @martin_sheen
+  end
+
+  it "must create children from scalars" do
+    @martin_sheen.new_child 'fake_emilio'
+    @martin_sheen.children.size.must_equal 1
+    @martin_sheen.children.first.value.must_equal 'fake_emilio'
+    @martin_sheen.children.wont_include @emilio_estevez
+  end
+end
