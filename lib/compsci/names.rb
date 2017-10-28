57a5d4d76b67e93f7167de59993effe7f6a74b93
Rick Hull <rick.hull@gmail.com>
2017-10-28 22:23:49 +0000

    add names module

---
+++ lib/compsci/names.rb
@@ -0,0 +1,123 @@
+# coding: utf-8
+module CompSci
+  module Names
+    WW1 = [:apples, :butter, :charlie, :duff, :edward, :freddy, :george,
+           :harry, :ink, :johnnie, :king, :london, :monkey, :nuts, :orange,
+           :pudding, :queenie, :robert, :sugar, :tommy, :uncle, :vinegar,
+           :willie, :xerxes, :yellow, :zebra]
+    WW2 = [:able, :baker, :charlie, :dog, :easy, :fox, :george, :how, :item,
+           :jig, :king, :love, :mike, :nan, :oboe, :peter, :queen, :roger,
+           :sugar, :tare, :uncle, :victor, :william, :xray, :yoke, :zebra]
+    NATO = [:alfa, :bravo, :charlie, :delta, :echo, :foxtrot, :golf, :hotel,
+            :india, :juliett, :kilo, :lima, :mike, :november, :oscar, :papa,
+            :quebec, :romeo, :sierra, :tango, :uniform, :victor, :whiskey,
+            :xray, :yankee, :zulu]
+    CRYPTO = [:alice, :bob, :charlie, :david, :eve, :frank, :grace, :heidi,
+              :judy, :mallory, :olivia, :peggy, :sybil, :trudy, :victor,
+              :wendy]
+    ENGLISH_UPPER = [*'A'..'Z']
+    ENGLISH_LOWER = [*'a'..'z']
+
+    PLANETS = [:mercury, :venus, :earth, :mars, :jupiter, :saturn, :uranus,
+               :neptune, :pluto]
+
+    SOLAR = [:mercury, :venus, :earth, :mars, :asteroid_belt, :jupiter,
+             :saturn, :uranus, :neptune, :kuiper_belt, :scattered_disk,
+             :heliosphere]
+
+    # map val to [0..names.length]
+    def self.assign(val, names)
+      case val
+      when String
+        if val.match %r{\A\d+\z}
+          pos = val.to_i
+        else
+          case names.size
+          when (1..26)
+            pos = val[0].upcase.ord - 'A'.ord
+          when (27..99)
+            pos = val[0].ord - 'A'.ord
+            pos -= 6 if pos > 26
+          else
+            raise "unexpected names.size: #{names.size}"
+          end
+        end
+      when Integer
+        pos = val
+      else
+        raise "unexpected val: #{val} (#{val.class})"
+      end
+
+      if pos < 0 or pos >= names.size
+        raise "val #{val} pos #{pos} outside of names range (#{names.size})"
+      end
+      names[pos]
+    end
+
+    module Greek
+      UPPER = [*'Α'..'Ρ', *'Σ'..'Ω']
+      LOWER = [*('α'..'ρ'), *('σ'..'ω')]
+      SYMBOLS = [:alpha, :beta, :gamma, :delta, :epsilon, :zeta, :eta, :theta,
+                 :iota, :kappa, :lambda, :mu, :nu, :xi, :omicron, :pi, :rho,
+                 :sigma, :tau, :upsilon, :phi, :chi, :psi, :omega]
+
+      # e.g. { alpha: ['Α', 'α'] ...
+      CHAR_MAP = {}
+      SYMBOLS.each_with_index { |sym, i| CHAR_MAP[sym] = [UPPER[i], LOWER[i]] }
+
+      LATIN_SYMBOLS = {
+        a: :alpha,
+        b: :beta,
+        c: :gamma,
+        d: :delta,
+        e: :epsilon,
+        f: :zeta,
+        g: :eta,
+        h: :theta,
+        i: :iota,
+        j: :xi,      # use for j, q and w
+        k: :kappa,
+        l: :lambda,
+        m: :mu,
+        n: :nu,
+        # nonesuch: => :xi,
+        o: :omicron,
+        p: :pi,
+        q: :xi,      # use for j, q and w
+        r: :rho,
+        s: :sigma,
+        t: :tau,
+        u: :upsilon,
+        v: :phi,
+        w: :xi,      # use for j, q and w
+        x: :chi,
+        y: :psi,
+        z: :omega,
+      }
+      SYMBOLS26 = LATIN_SYMBOLS.values
+
+      def self.upper(latin_str)
+        CHAR_MAP.fetch(self.sym(latin_str)).first
+      end
+
+      def self.lower(latin_str)
+        CHAR_MAP.fetch(self.sym(latin_str)).last
+      end
+
+      def self.sym(val)
+        case val
+        when String
+          LATIN_SYMBOLS.fetch val[0].downcase.to_sym
+        when Integer
+          if val >= 0 and val < SYMBOLS.size
+            SYMBOLS[val]
+          else
+            raise "val #{val} not in range (#{SYMBOLS.size})"
+          end
+        else
+          raise "unexpected val #{val} (#{val.class})"
+        end
+      end
+    end
+  end
+end
---
+++ test/names.rb
@@ -0,0 +1,78 @@
+require 'compsci/names'
+require 'minitest/autorun'
+
+include CompSci
+
+describe Names do
+  describe "alphabetic constants" do
+    it "must have size 26" do
+      Names::WW1.size.must_equal 26
+      Names::WW2.size.must_equal 26
+      Names::NATO.size.must_equal 26
+      Names::ENGLISH_UPPER.size.must_equal 26
+      Names::ENGLISH_LOWER.size.must_equal 26
+    end
+  end
+
+  describe "Names.assign" do
+    it "must handle English / ASCII strings" do
+      upper_lower = Names::ENGLISH_UPPER + Names::ENGLISH_LOWER
+      Names.assign('cat', Names::ENGLISH_UPPER).must_equal 'C'
+      Names.assign('Cat', Names::ENGLISH_UPPER).must_equal 'C'
+      Names.assign('cat', Names::ENGLISH_LOWER).must_equal 'c'
+      Names.assign('Cat', Names::ENGLISH_LOWER).must_equal 'c'
+      Names.assign('Cat', upper_lower).must_equal 'C'
+      Names.assign('cat', upper_lower).must_equal 'c'
+      Names.assign('cat', Names::NATO).must_equal :charlie
+      Names.assign('Cat', Names::NATO).must_equal :charlie
+      Names.assign('Dog', Names::CRYPTO).must_equal :david
+    end
+
+    it "must handle integers" do
+      upper_lower = Names::ENGLISH_UPPER + Names::ENGLISH_LOWER
+      Names.assign(36, upper_lower).must_equal 'k'
+      Names.assign(0, upper_lower).must_equal 'A'
+      Names.assign(0, Names::ENGLISH_UPPER).must_equal 'A'
+      Names.assign(3, Names::ENGLISH_LOWER).must_equal 'd'
+    end
+  end
+
+  describe Names::Greek do
+    describe "greek alphabetic constants" do
+      it "must have size 24" do
+        Names::Greek::UPPER.size.must_equal 24
+        Names::Greek::LOWER.size.must_equal 24
+        Names::Greek::SYMBOLS.size.must_equal 24
+        Names::Greek::CHAR_MAP.size.must_equal 24
+      end
+    end
+  end
+
+  describe "Greek.sym" do
+    it "must handle strings and integers" do
+      Names::Greek.sym('cat').must_equal :gamma
+      Names::Greek.sym('Cat').must_equal :gamma
+      Names::Greek.sym('zeta').must_equal :omega
+      Names::Greek.sym(0).must_equal :alpha
+      Names::Greek.sym(23).must_equal :omega
+    end
+  end
+
+  describe "Greek.lower" do
+    it "must handle strings and integers" do
+      third = Names::Greek::LOWER[2]
+      Names::Greek.lower('cat').must_equal third
+      Names::Greek.lower('Cat').must_equal third
+      Names::Greek.lower(2).must_equal third
+    end
+  end
+
+  describe "Greek.upper" do
+    it "must handle strings and integers" do
+      fourth = Names::Greek::UPPER[3]
+      Names::Greek.upper('dog').must_equal fourth
+      Names::Greek.upper('Dog').must_equal fourth
+      Names::Greek.upper(3).must_equal fourth
+    end
+  end
+end
