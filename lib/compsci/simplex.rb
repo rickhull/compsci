1c144a5d68643cf3830de0f92cb0755d1780ec25
Rick Hull <rick.hull@gmail.com>
2017-10-31 07:51:13 +0000

    add CompSci::Simplex

---
+++ lib/compsci/simplex.rb
@@ -0,0 +1,172 @@
+require 'compsci'
+
+# note, this work is based on https://github.com/rickhull/simplex
+# which was forked in 2017 from https://github.com/danlucraft/simplex
+# which had its last commit in 2013
+
+class CompSci::Simplex
+  DEFAULT_MAX_PIVOTS = 10_000
+
+  class Error < RuntimeError; end
+  class UnboundedProblem < Error; end
+  class SanityCheck < Error; end
+  class TooManyPivots < Error; end
+
+  attr_accessor :max_pivots
+
+  # c - coefficients of objective function; size: num_vars
+  # a - inequality lhs coefficients;   2dim size: num_inequalities, num_vars
+  # b - inequality rhs constants            size: num_inequalities
+  def initialize(c, a, b)
+    num_vars = c.size
+    num_inequalities = b.size
+    raise(ArgumentError, "a doesn't match b") unless a.size == num_inequalities
+    raise(ArgumentError, "a doesn't match c") unless a.first.size == num_vars
+
+    @max_pivots = DEFAULT_MAX_PIVOTS
+
+    # Problem dimensions; these never change
+    @num_non_slack_vars = num_vars
+    @num_constraints    = num_inequalities
+    @num_vars           = @num_non_slack_vars + @num_constraints
+
+    # Set up initial matrix A and vectors b, c
+    @c = c.map { |flt| -1 * flt } + Array.new(@num_constraints, 0)
+    @a = a.map.with_index { |ary, i|
+      if ary.size != @num_non_slack_vars
+        raise ArgumentError, "a is inconsistent"
+      end
+      ary + Array.new(@num_constraints) { |ci| ci == i ? 1 : 0 }
+    }
+    @b = b
+
+    # set initial solution: all non-slack variables = 0
+    @basic_vars = (@num_non_slack_vars...@num_vars).to_a
+    self.update_solution
+  end
+
+  # does not modify vector / matrix
+  def update_solution
+    @x = Array.new(@num_vars, 0)
+
+    @basic_vars.each { |basic_var|
+      idx = nil
+      @num_constraints.times { |i|
+        if @a[i][basic_var] == 1
+          idx =i
+          break
+        end
+      }
+      raise(SanityCheck, "no idx for basic_var #{basic_var} in a") unless idx
+      @x[basic_var] = @b[idx]
+    }
+  end
+
+  def solution
+    self.solve
+    self.current_solution
+  end
+
+  def solve
+    count = 0
+    while self.can_improve?
+      count += 1
+      raise(TooManyPivots, count.to_s) unless count < @max_pivots
+      self.pivot
+    end
+  end
+
+  def current_solution
+    @x[0...@num_non_slack_vars]
+  end
+
+  def can_improve?
+    !self.entering_variable.nil?
+  end
+
+  # idx of @c's minimum negative value
+  # nil when no improvement is possible
+  #
+  def entering_variable
+    (0...@c.size).select { |i| @c[i] < 0 }.min_by { |i| @c[i] }
+  end
+
+  def pivot
+    pivot_column = self.entering_variable or return nil
+    pivot_row = self.pivot_row(pivot_column) or raise UnboundedProblem
+    leaving_var = nil
+    @a[pivot_row].each_with_index { |a, i|
+      if a == 1 and @basic_vars.include?(i)
+        leaving_var = i
+        break
+      end
+    }
+    raise(SanityCheck, "no leaving_var") if leaving_var.nil?
+
+    @basic_vars.delete(leaving_var)
+    @basic_vars.push(pivot_column)
+    @basic_vars.sort!
+
+    pivot_ratio = Rational(1, @a[pivot_row][pivot_column])
+
+    # update pivot row
+    @a[pivot_row] = @a[pivot_row].map { |val| val * pivot_ratio }
+    @b[pivot_row] = @b[pivot_row] * pivot_ratio
+
+    # update objective
+    # @c -= @c[pivot_column] * @a[pivot_row]
+    @c = @c.map.with_index { |val, i|
+      val - @c[pivot_column] * @a[pivot_row][i]
+    }
+
+    # update A and B
+    @num_constraints.times { |i|
+      next if i == pivot_row
+      r = @a[i][pivot_column]
+      @a[i] = @a[i].map.with_index { |val, j| val - r * @a[pivot_row][j] }
+      @b[i] = @b[i] - r * @b[pivot_row]
+    }
+
+    self.update_solution
+  end
+
+  def pivot_row(column_ix)
+    min_ratio = nil
+    idx = nil
+    @num_constraints.times { |i|
+      a, b = @a[i][column_ix], @b[i]
+      next if a == 0 or (b < 0) ^ (a < 0)
+      ratio = Rational(b, a)
+      idx, min_ratio = i, ratio if min_ratio.nil? or ratio <= min_ratio
+    }
+    idx
+  end
+
+  def formatted_tableau
+    if self.can_improve?
+      pivot_column = self.entering_variable
+      pivot_row    = self.pivot_row(pivot_column)
+    else
+      pivot_row = nil
+    end
+    c = @c.to_a.map { |flt| "%2.3f" % flt }
+    b = @b.to_a.map { |flt| "%2.3f" % flt }
+    a = @a.to_a.map { |vec| vec.to_a.map { |flt| "%2.3f" % flt } }
+    if pivot_row
+      a[pivot_row][pivot_column] = "*" + a[pivot_row][pivot_column]
+    end
+    max = (c + b + a + ["1234567"]).flatten.map(&:size).max
+    result = []
+    result << c.map { |str| str.rjust(max, " ") }
+    a.zip(b) do |arow, brow|
+      result << (arow + [brow]).map { |val| val.rjust(max, " ") }
+      result.last.insert(arow.length, "|")
+    end
+    lines = result.map { |ary| ary.join("  ") }
+    max_line_length = lines.map(&:length).max
+    lines.insert(1, "-"*max_line_length)
+    lines.join("\n")
+  end
+
+
+end
---
+++ test/simplex.rb
@@ -0,0 +1,291 @@
+require 'minitest/autorun'
+require 'compsci/simplex'
+
+include CompSci
+
+class SimplexTest < Minitest::Test
+  def test_2x2
+    result = Simplex.new(
+      [1, 1],
+      [
+        [ 2,  1],
+        [ 1,  2],
+      ],
+      [4, 3]
+    ).solution
+    assert_equal [Rational(5, 3), Rational(2, 3)], result
+  end
+
+  def test_2x2_b
+    result = Simplex.new(
+      [3, 4],
+      [
+        [ 1,  1],
+        [ 2,  1],
+      ],
+      [4, 5]
+    ).solution
+    assert_equal [0, 4], result
+  end
+
+  def test_2x2_c
+    result = Simplex.new(
+      [2, -1],
+      [
+        [ 1,  2],
+        [ 3,  2],
+      ],
+      [6, 12]
+    ).solution
+    assert_equal [4, 0], result
+  end
+
+  def test_3x3_a
+    result = Simplex.new(
+      [60, 90, 300],
+      [
+        [1, 1, 1],
+        [1, 3, 0],
+        [2, 0, 1]
+      ],
+      [600, 600, 900]
+    ).solution
+    assert_equal [0, 0, 600], result
+  end
+
+  def test_3x3_b
+    result = Simplex.new(
+      [70, 210, 140],
+      [
+        [ 1,  1,  1],
+        [ 5,  4,  4],
+        [40, 20, 30]
+      ],
+      [100, 480, 3200]
+    ).solution
+    assert_equal [0, 100, 0], result
+  end
+
+  def test_3x3_c
+    result = Simplex.new(
+      [2, -1, 2],
+      [
+        [ 2,  1,  0],
+        [ 1,  2, -2],
+        [ 0,  1,  2]
+      ],
+      [10, 20, 5]
+    ).solution
+    assert_equal [5, 0, Rational(5, 2)], result
+  end
+
+  def test_3x3_d
+    result = Simplex.new(
+      [11, 16, 15],
+      [
+        [             1,              2, Rational(3, 2)],
+        [Rational(2, 3), Rational(2, 3),              1],
+        [Rational(1, 2), Rational(1, 3), Rational(1, 2)]
+      ],
+      [12_000, 4_600, 2_400]
+    ).solution
+    assert_equal [600, 5_100, 800], result
+  end
+
+  def test_3x3_e
+    simplex = Simplex.new(
+      [5, 4, 3],
+      [
+        [2, 3, 1],
+        [4, 1, 2],
+        [3, 4, 2]
+      ],
+      [5, 11, 8]
+    )
+    assert_equal [2, 0, 1], simplex.solution
+  end
+
+  def test_3x3_f
+    simplex = Simplex.new(
+      [3, 2, -4],
+      [
+        [1, 4, 0],
+        [2, 4,-2],
+        [1, 1,-2]
+      ],
+      [5, 6, 2]
+    )
+    assert_equal [4, 0, 1], simplex.solution
+  end
+
+  def test_3x3_g
+    simplex = Simplex.new(
+      [2, -1, 8],
+      [
+        [2, -4, 6],
+        [-1, 3, 4],
+        [0, 0, 2]
+      ],
+      [3, 2, 1]
+    )
+    assert_equal [Rational(17, 2), Rational(7,2), 0], simplex.solution
+  end
+
+  def test_3x4
+    result = Simplex.new(
+      [100_000, 40_000, 18_000],
+      [
+        [20, 6, 3],
+        [ 0, 1, 0],
+        [-1,-1, 1],
+        [-9, 1, 1]
+      ],
+      [182, 10, 0, 0]
+    ).solution
+    assert_equal [4, 10, 14], result
+  end
+
+  def test_4x4
+    result = Simplex.new(
+      [1, 2, 1, 2],
+      [
+        [1, 0, 1, 0],
+        [0, 1, 0, 1],
+        [1, 1, 0, 0],
+        [0, 0, 1, 1]
+      ],
+      [1, 4, 2, 2]
+    ).solution
+    assert_equal [0, 2, 0, 2], result
+  end
+
+  def test_cycle
+    result = Simplex.new(
+      [10, -57, -9, -24],
+      [
+        [0.5, -5.5, -2.5, 9],
+        [0.5, -1.5, -0.5, 1],
+        [  1,    0,    0, 0]
+      ],
+      [0, 0, 1]
+    ).solution
+    assert_equal [1, 0, 1, 0], result
+  end
+
+  def test_cycle2
+    simplex = Simplex.new(
+      [2, 3, -1, -12],
+      [
+        [-2, -9, 1, 9],
+        [Rational(1, 3), 1, Rational(-1, 3), -2],
+      ],
+      [0, 0]
+    )
+    assert_raises Simplex::UnboundedProblem do
+      simplex.solution
+    end
+  end
+
+  def test_error_mismatched_dimensions
+    assert_raises ArgumentError do
+      Simplex.new(
+        [10, -57, -9],
+        [
+          [0.5, -5.5, -2.5, 9],
+          [0.5, -1.5, -0.5, 1],
+          [  1,    0,    0, 0]
+        ],
+        [0, 0, 1]
+      )
+    end
+
+    assert_raises ArgumentError do
+      Simplex.new(
+        [10, -57, -9, 2],
+        [
+          [0.5, -5.5, 9, 4],
+          [0.5, -1.5, 1],
+          [  1,    0, 0]
+        ],
+        [0, 0, 1]
+      )
+    end
+
+    assert_raises ArgumentError do
+      Simplex.new(
+        [10, -57, -9, 2],
+        [
+          [0.5, -5.5, 9, 4],
+          [0.5, -1.5, 1, 5],
+          [  1,    0, 0, 5]
+        ],
+        [0, 1]
+      )
+    end
+  end
+
+  def test_manual_iteration
+    simplex = Simplex.new(
+      [10, -57, -9, -24],
+      [
+        [0.5, -5.5, -2.5, 9],
+        [0.5, -1.5, -0.5, 1],
+        [  1,    0,    0, 0]
+      ],
+      [0, 0, 1]
+    )
+    while simplex.can_improve?
+      assert simplex.formatted_tableau.is_a?(String)
+      simplex.pivot
+    end
+    result = simplex.solution
+    assert_equal [1, 0, 1, 0], result
+  end
+
+  def test_cup_factory
+    result = Simplex.new(
+      [25, 20],
+      [
+        [20, 12],
+        [1, 1]
+      ],
+      [1800, 8*15]
+    )
+    assert_equal [45, 75], result.solution
+  end
+
+  #def test_infeasible1
+  #  simplex = Simplex.new(
+  #    [2, -1],
+  #    [
+  #      [1, -1],
+  #      [-1, 1]
+  #    ],
+  #    [1, -2]
+  #  )
+  #  while simplex.can_improve?
+  #    puts
+  #    puts simplex.formatted_tableau
+  #    simplex.pivot
+  #  end
+  #  p :done
+  #  puts
+  #  puts simplex.formatted_tableau
+
+  #end
+
+  def test_unbounded
+    simplex = Simplex.new(
+      [1, 1, 1],
+      [
+        [3, 1, -2],
+        [4, 3, 0]
+      ],
+      [5, 7]
+    )
+    assert_raises Simplex::UnboundedProblem do
+      simplex.solution
+    end
+  end
+
+end
