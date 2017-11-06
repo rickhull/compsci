611ab8c03a3cfd55091ec28fd89c1fc86aa62308
Rick Hull <rick.hull@gmail.com>
2017-11-06 07:27:02 +0000

    finish *FlexNode conversion

--- test/bench/tree.rb
+++ test/bench/tree.rb
@@ -1,3 +1,4 @@
+require 'compsci/node'
 require 'compsci/tree'
 require 'benchmark/ips'
 
@@ -7,22 +8,22 @@ Benchmark.ips do |b|
   b.config time: 3, warmup: 0.5
 
   b.report("99x BinaryTree(ChildNode)#push") do
-    tree = BinaryTree.new(ChildNode, 42)
+    tree = BinaryTree.new(ChildFlexNode, 42)
     99.times { tree.push rand 99 }
   end
 
-  b.report("99x BinaryTree(Node)#push") do
-    tree = BinaryTree.new(Node, 42)
+  b.report("99x BinaryTree(FlexNode)#push") do
+    tree = BinaryTree.new(FlexNode, 42)
     99.times { tree.push rand 99 }
   end
 
-  b.report("99x TernaryTree(ChildNode)#push") do
-    tree = TernaryTree.new(ChildNode, 42)
+  b.report("99x TernaryTree(ChildFlexNode)#push") do
+    tree = TernaryTree.new(ChildFlexNode, 42)
     99.times { tree.push rand 99 }
   end
 
-  b.report("99x TernaryTree(Node)#push") do
-    tree = TernaryTree.new(Node, 42)
+  b.report("99x TernaryTree(FlexNode)#push") do
+    tree = TernaryTree.new(FlexNode, 42)
     99.times { tree.push rand 99 }
   end
 
