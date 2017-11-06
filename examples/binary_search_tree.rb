cd4e82ba5f8c2ef024effa6eb2abaa846ca61dfa
Rick Hull <rick.hull@gmail.com>
2017-11-06 22:49:22 +0000

    BST KeyNodes work

--- examples/binary_search_tree.rb
+++ examples/binary_search_tree.rb
@@ -1,12 +1,16 @@
 require 'compsci/node'
 require 'compsci/binary_search_tree'
+require 'compsci/names'
 
 include CompSci
 
 RANDMAX = 99
 
-p vals = Array.new(15) { rand RANDMAX }
+p vals = Names::WW1.shuffle
+p keys = Array.new(vals.size) { rand RANDMAX }
 
-b = BinarySearchTree.new vals.shift
-b.insert vals.shift until vals.empty?
-puts b
+root = BinarySearchTree.new_node(keys.shift, vals.shift)
+tree = BinarySearchTree.new(root)
+tree[keys.shift] = vals.shift until keys.empty?
+# tree.insert(keys.shift, vals.shift) until keys.empty?
+puts tree
--- lib/compsci/binary_search_tree.rb
+++ lib/compsci/binary_search_tree.rb
@@ -1,6 +1,20 @@
+require 'compsci/node'
 require 'compsci/tree'
 
 module CompSci
+  class KeyNode < Node
+    attr_accessor :key
+
+    def initialize(val, key: nil, children: [])
+      @key = key
+      super(val, children: children)
+    end
+
+    def to_s
+      [key, value].join(':')
+    end
+  end
+
   class BinarySearchTree < BinaryTree
     def self.display_level(nodes: [], width: 80)
       unless self.power_of?(nodes.size, 2)
@@ -14,42 +28,56 @@ module CompSci
       }.join
     end
 
-    def initialize(val)
-      @root = Node.new(val, children: 2)
+    def self.new_node(key, val)
+      KeyNode.new(val, key: key, children: 2)
+    end
+
+    # ARGH, can't decide between passing klass, *args
+    # or passing in an instantiated Node as root
+    # We already started with the latter and shifted to the former
+    # I'm tempted to revert to the latter
+    def initialize(root_node)
+      @node_class = root_node.class
       @child_slots = 2
+      if root_node.children.size == @child_slots
+        @root = root_node
+      else
+        raise "bad root: #{root_node}; expected 2 child slots"
+      end
     end
 
-    def search_recursive(val, node: @root)
-      return node if node.nil? or node.value == val
-      child = node.value < val ? node.children[0] : node.children[1]
-      search_recursive(val, node: child)
+    def search_recursive(key, node: @root)
+      return node if node.nil? or node.key == key
+      child = node.key < key ? node.children[0] : node.children[1]
+      search_recursive(key, node: child)
     end
 
-    def search_iterative(val, node: @root)
+    def search_iterative(key, node: @root)
       while !node.nil?
-        return node if node.value == val
-        node = node.value < val ? node.children[0] : node.children[1]
+        return node if node.key == key
+        node = node.key < key ? node.children[0] : node.children[1]
       end
       node
     end
 
-    def insert_recursive(val, node: @root)
-      return nil if node.nil? or node.value == val
-      if val < node.value
+    def insert_recursive(key, val, node: @root)
+      return nil if node.nil? or node.key == key
+      if key < node.key
         if node.children[0]
-          insert_recursive(val, node: node.children[0])
+          insert_recursive(key, val, node: node.children[0])
         else
-          node.children[0] = Node.new(val, children: 2)
+          node.children[0] = @node_class.new(val, key: key, children: 2)
         end
       else
         if node.children[1]
-          insert_recursive(val, node: node.children[1])
+          insert_recursive(key, val, node: node.children[1])
         else
-          node.children[1] = Node.new(val, children: 2)
+          node.children[1] = @node_class.new(val, key: key, children: 2)
         end
       end
     end
     alias_method :insert, :insert_recursive
+    alias_method :[]=, :insert
 
     def display(node: @root, width: 80)
       levels = [self.class.display_level(nodes: [node], width: width)]
