f751668f697cf110ed48410a89893512ff2e6e1b
Rick Hull <rick.hull@gmail.com>
2017-11-19 00:23:41 +0000

    move all PushTree stuff to examples/tree_push.rb

--- examples/tree_push.rb
+++ examples/tree_push.rb
@@ -6,6 +6,45 @@ include CompSci
 
 puts <<EOF
 #
+# Try out Binary, Ternary, and Quaternary PushTrees
+# Push the same vals to each
+#
+
+EOF
+
+vals = Array.new(30) { rand 99 }
+
+[2, 3, 4].each { |children|
+  my_vals = vals.dup
+  p my_vals
+
+  root = ChildFlexNode.new my_vals.shift
+  tree = PushTree.new(root, child_slots: children)
+  tree.push my_vals.shift until my_vals.empty?
+  p tree
+  puts root.display(width: 80)
+  puts
+  visited = []
+  tree.df_search { |n|
+    visited << n
+    false # or n.value > 90
+  }
+  puts "df_search visited: %s" % visited.join(' ')
+  puts
+  puts
+
+  # push different vals for each class
+  my_vals = Array.new(30) { rand 99 }
+  puts "push: #{my_vals.inspect}"
+  tree.push my_vals.shift until my_vals.empty?
+  puts
+  puts root.display(width: 80)
+  puts
+  puts
+}
+
+puts <<EOF
+#
 # 30 inserts, puts, df_search
 #
 
