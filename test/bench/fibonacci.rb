403239387ca30069f4ff56f0605045a9117f0291
Rick Hull <rick.hull@gmail.com>
2017-11-19 21:37:49 +0000

    just use benchmark/ips for fib bench for now

--- test/bench/fibonacci.rb
+++ test/bench/fibonacci.rb
@@ -1,82 +1,44 @@
 require 'compsci/fibonacci'
-require 'minitest/autorun'
+require 'benchmark/ips'
 
 include CompSci
 
-# CLASSIC_RANGE = [3, 5, 10, 15, 20, 25, 30, 31, 32, 33, 34, 35]
-CLASSIC_RANGE = [10, 15, 20, 25, 30, 31, 32, 33, 34, 35]
-# RECURSIVE_RANGE = [10, 100, 1000, 2500, 5000, 7500]
-RECURSIVE_RANGE = [100, 1000, 2500, 5000, 7500]
-# CACHE_RANGE = [100, 1000, 10000, 100000, 112500, 125000]
-CACHE_RANGE = [100, 1000, 10000, 100000]
+# recursive benchmarks with low N; iterative for comparison
+Benchmark.ips do |b|
+  b.config time: 3, warmup: 0.5
+  num = 25
 
-# this causes churn at the process level and impacts other benchmarks
-# DYNAMIC_RANGE = [100, 1000, 10000, 100000, 200000, 500000]
-DYNAMIC_RANGE = [100, 1000, 10000, 100000]
-MATRIX_RANGE = [100, 1000, 10000, 100000]
+  b.report("Fibonacci.classic(#{num})") {
+    Fibonacci.classic(num)
+  }
 
-#CLASS_BENCHMARK = true
-BENCHMARK_IPS = true
-CLASS_BENCHMARK = false
-#BENCHMARK_IPS = false
+  b.report("Fibonacci.cache_recursive(#{num})") {
+    Fibonacci.cache_recursive(num)
+  }
 
-if CLASS_BENCHMARK
-  require 'compsci/timer'
+  b.report("Fibonacci.cache_iterative(#{num})") {
+    Fibonacci.cache_iterative(num)
+  }
 
-  class BenchFib < Minitest::Benchmark
-    def bench_fib
-      times = CLASSIC_RANGE.map { |n|
-        _answer, elapsed = Timer.elapsed { Fibonacci.classic(n) }
-        elapsed
-      }
-      _a, _b, r2 = self.fit_exponential(CLASSIC_RANGE, times)
-      puts
-      puts "self-timed Fibonacci.classic(n) exponential fit: %0.3f" % r2
-      puts
-    end
-  end
+  b.compare!
 end
 
-if BENCHMARK_IPS
-  require 'benchmark/ips'
+# nonrecursive benchmarks with high N
+Benchmark.ips do |b|
+  b.config time: 3, warmup: 0.5
+  num = 500
 
-  # recursive benchmarks with low N; iterative for comparison
-  Benchmark.ips do |b|
-    b.config time: 3, warmup: 0.5
-    num = 25
+  b.report("Fibonacci.cache_iterative(#{num})") {
+    Fibonacci.cache_iterative(num)
+  }
 
-    b.report("Fibonacci.classic(#{num})") {
-      Fibonacci.classic(num)
-    }
+  b.report("Fibonacci.dynamic(#{num})") {
+    Fibonacci.dynamic(num)
+  }
 
-    b.report("Fibonacci.cache_recursive(#{num})") {
-      Fibonacci.cache_recursive(num)
-    }
+  b.report("Fibonacci.matrix(#{num})") {
+    Fibonacci.matrix(num)
+  }
 
-    b.report("Fibonacci.cache_iterative(#{num})") {
-      Fibonacci.cache_iterative(num)
-    }
-
-    b.compare!
-  end
-
-  # nonrecursive benchmarks with high N
-  Benchmark.ips do |b|
-    b.config time: 3, warmup: 0.5
-    num = 500
-
-    b.report("Fibonacci.cache_iterative(#{num})") {
-      Fibonacci.cache_iterative(num)
-    }
-
-    b.report("Fibonacci.dynamic(#{num})") {
-      Fibonacci.dynamic(num)
-    }
-
-    b.report("Fibonacci.matrix(#{num})") {
-      Fibonacci.matrix(num)
-    }
-
-    b.compare!
-  end
+  b.compare!
 end
