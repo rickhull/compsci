251f640aaa8db30a0521e12cd37917cadf52006f
Rick Hull <rick.hull@gmail.com>
2017-11-19 00:24:05 +0000

    just use Node, not Tree

--- examples/tree.rb
+++ examples/tree.rb
@@ -1,43 +1,34 @@
 require 'compsci/node'
-require 'compsci/tree'
-require 'compsci/timer'
 
 include CompSci
 
 puts <<EOF
 #
-# Try out Binary-, Ternary-, and QuaternaryTree
+# Fill up and display some trees
 #
 
 EOF
 
 vals = Array.new(30) { rand 99 }
+p vals
 
-# [BinaryTree, TernaryTree, QuaternaryTree].each { |tree_class|
-[2, 3, 4].each { |slots|
-  # start with the same vals for each class
+[2, 3, 4].each { |children|
   my_vals = vals.dup
-  p my_vals
-  root = ChildFlexNode.new my_vals.shift
-  tree = PushTree.new(root, child_slots: slots)
-  tree.push my_vals.shift until my_vals.empty?
-  p tree
-  puts root.display(width: 80)
-  puts
-  visited = []
-  tree.df_search { |n|
-    visited << n
-    false # or n.value > 90
-  }
-  puts "df_search visited: %s" % visited.join(' ')
-  puts
-  puts
 
-  # push different vals for each class
-  my_vals = Array.new(30) { rand 99 }
-  puts "push: #{my_vals.inspect}"
-  tree.push my_vals.shift until my_vals.empty?
+  root = Node.new my_vals.shift, children: children
+  nodes = [root]
+  until my_vals.empty?
+    new_nodes = []
+    nodes.each { |node|
+      children.times { |i|
+        node[i] = Node.new my_vals.shift, children: children
+      }
+      new_nodes += node.children
+    }
+    nodes = new_nodes
+  end
   puts
+  p root
   puts root.display(width: 80)
   puts
   puts
