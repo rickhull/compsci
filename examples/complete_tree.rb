765493d74d80896393b743a0a9dc472cb646cbfe
Rick Hull <rick.hull@gmail.com>
2017-11-19 21:17:26 +0000

    improve output for examples/complete_tree.rb

--- examples/complete_tree.rb
+++ examples/complete_tree.rb
@@ -1,46 +1,41 @@
 require 'compsci/complete_tree'
-require 'compsci/timer'
 
 include CompSci
-
-puts <<EOF
-#
-# Print CompleteBinary-, Ternary-, and QuaternaryTree
-#
-
-EOF
 
 vals = Array.new(30) { rand 99 }
 
 [CompleteBinaryTree,
  CompleteTernaryTree,
  CompleteQuaternaryTree].each { |tree_class|
+
+  puts <<EOF
+
+#
+# Print #{tree_class}
+#
+
+EOF
+
   # start with the same vals for each class
   my_vals = vals.dup
-  p my_vals
+  puts "initial vals: #{my_vals.inspect}"
   tree = tree_class.new
   tree.push my_vals.shift until my_vals.empty?
+
   p tree
+  puts
   puts tree.display(width: 80)
   puts
   puts
-  puts
 
-
-  # TODO: add CompleteTree#df_search
-  # tree.df_search { |n|
-  #   puts "visited #{n}"
-  #   false # or n.value > 90
-  # }
-  # puts
 
   # push different vals for each class
   my_vals = Array.new(30) { rand 99 }
-  puts "push: #{my_vals.inspect}"
+  puts "new vals: #{my_vals.inspect}"
 
   tree.push my_vals.shift until my_vals.empty?
-  puts tree.display(width: 80)
   puts
+  puts tree.display(width: 80)
   puts
   puts
 }
