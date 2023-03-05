7cae440df6b64bcf29b218a00b9bfdb23f18e2ae
Rick Hull <rick.hull@gmail.com>
2023-03-05 17:08:26 -0500

    relax some testing thresholds for timer tests
    * CI platforms may be overloaded and cause failing tests
    * stop using must_be_close_to
    * prefer using must_be :> and must_be :<

--- test/timer.rb
+++ test/timer.rb
@@ -5,10 +5,11 @@ include CompSci
 
 describe Timer do
   describe "elapsed" do
-    it "must return the block value and positive number" do
+    it "returns the block value and a positive number" do
       answer, elapsed = Timer.elapsed { sleep 0.01; :foo }
       expect(answer).must_equal :foo
-      expect(elapsed).must_be_close_to 0.015, 0.01
+      expect(elapsed).must_be :>, 0
+      expect(elapsed).must_be :<, 0.1
     end
   end
 
@@ -16,43 +17,50 @@ describe Timer do
     it "must be positive" do
       start = Timer.now
       sleep 0.01
-      expect(Timer.since(start)).must_be_close_to 0.015, 0.01
+      elapsed = Timer.since(start)
+      expect(elapsed).must_be :>, 0
+      expect(elapsed).must_be :<, 0.1
     end
   end
 
   describe "loop_avg" do
-    it "return the block value and a positive number" do
+    it "returns the block value and a positive number" do
       start = Timer.now
       answer, avg_et = Timer.loop_avg(seconds: 0.1) {
         sleep 0.01
         :foo
       }
+      elapsed = Timer.since(start)
       expect(answer).must_equal :foo
-      expect(avg_et).must_be_close_to 0.01, 0.005
-      expect(Timer.since(start)).must_be_close_to 0.15, 0.05
+      expect(avg_et).must_be :>, 0.005
+      expect(avg_et).must_be :<, 0.05
+      expect(elapsed).must_be :>, 0.05
+      expect(elapsed).must_be :<, 0.5
     end
 
-    it "must repeat short loops and stop on time" do
-      # see above, Timer.since(start)
-      true
-    end
-
-    it "must cease looping after 5 loops" do
+    it "ceases looping after 5 loops" do
       start = Timer.now
       _answer, avg_et = Timer.loop_avg(count: 5) {
         sleep 0.01
       }
-      expect(avg_et).must_be_close_to 0.015, 0.005
-      expect(Timer.since(start)).must_be_close_to 0.1, 0.05
+      elapsed = Timer.since(start)
+      expect(avg_et).must_be :>, 0.005
+      expect(avg_et).must_be :<, 0.05
+      expect(elapsed).must_be :>, 0.01
+      expect(elapsed).must_be :<, 0.1
     end
 
-    it "must not interrupt long loops" do
+    it "won't interrupt long loops" do
       start = Timer.now
       _answer, avg_et = Timer.loop_avg(seconds: 0.01) {
         sleep 0.1
       }
-      expect(Timer.since(start)).must_be_close_to avg_et, 0.05
-      expect(avg_et).must_be_close_to 0.15, 0.05
+      elapsed = Timer.since(start)
+
+      expect(avg_et).must_be :>, 0.05
+      expect(avg_et).must_be :<, 0.5
+      expect(elapsed).must_be :>, 0.05
+      expect(elapsed).must_be :<, 0.5
     end
   end
 end
