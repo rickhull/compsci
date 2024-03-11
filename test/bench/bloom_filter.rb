f9df80dfe79cc048c18413899486249a62ae9d40
Rick Hull <rick.hull@gmail.com>
2024-03-11 14:30:57 -0400

    revise params, structure, reporting

--- test/bench/bloom_filter.rb
+++ test/bench/bloom_filter.rb
@@ -3,19 +3,16 @@ require 'benchmark/ips' # gem
 
 include CompSci
 
-iters = 499
+iters = 999
 
 Benchmark.ips do |b|
   b.config(time: 2, warmup: 0.5)
 
   [BloomFilter, BloomFilter::Digest, BloomFilter::OpenSSL].each { |klass|
-    b.report("#{klass}") {
-      bitpow = [8, 10, 16, 24]
-      aspects = [3, 5, 7, 9, 11]
-
-      bitpow.each { |p|
-        aspects.each { |a|
-          bf = klass.new(bits: 2**p, aspects: a)
+    [3, 5, 7, 9, 11].each { |aspect|
+      b.report("#{klass} (#{aspect} aspects)") {
+        [8, 10, 16, 24].each { |bitpow|
+          bf = klass.new(bits: 2**bitpow, aspects: aspect)
           iters.times { |i| bf.add(i.to_s) }
           iters.times {     bf.include?(rand(iters * 2).to_s) }
         }
