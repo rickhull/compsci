3c13e7b6c2dfaa460cddb675320b42ebd96d943c
Rick Hull <rick.hull@gmail.com>
2024-06-09 19:21:14 -0400

    change Elo from module to class

--- lib/compsci/elo.rb
+++ lib/compsci/elo.rb
@@ -1,28 +1,70 @@
 require 'compsci'
 
 module CompSci
-  module Elo
-    INITIAL = 1500
-    K = 32
-    C = 400
-
+  class Elo
     # rating_a and rating_b are positive numbers
     def self.expected(rating_a, rating_b)
-      CompSci.positive!(rating_a) and CompSci.positive!(rating_b)
-      1 / (1 + 10**((rating_b - rating_a) / C))
+      DEFAULT.expected(rating_a, rating_b)
     end
 
     # outcome_a is number between 0 and 1
     # 1 indicates a "win" for A, 0 indicates a "win" for B
-    # 0.5 indicates a draw, and a number like 0.6 might indicate
-    # a "win" for A, like best 3 sets out of 5.
+    # 0.5 indicates a draw
+    # 0.6 might indicate a "win" for A, like best 3 sets out of 5.
     def self.update(rating_a, rating_b, outcome_a)
-      raise(ArgumentError, outcome_a.inspect) unless (0..1).include? outcome_a
+      DEFAULT.update(rating_a, rating_b, outcome_a)
+    end
+
+    attr_reader :initial, :k, :c
+
+    def initialize(initial: 1000, k: 32, c: 480)
+      @initial = initial
+      @k = k
+      @c = c.to_f
+    end
+
+    CLASSIC = { initial: 1500, k: 32, c: 400 }
+    MODERN = { initial: 1000, k: 32, c: 480 }
+    DEFAULT = self.new(**MODERN)
+
+    def expected(rating_a, rating_b)
+      CompSci.positive!(rating_a) and CompSci.positive!(rating_b)
+      1 / (1 + 10**((rating_b - rating_a) / @c))
+    end
+
+    def update(rating_a, rating_b, outcome)
+      raise(ArgumentError, outcome.inspect) unless (0..1).include? outcome
       exp_a = expected(rating_a, rating_b)
       exp_b = expected(rating_b, rating_a)
 
-      [rating_a + K * (outcome_a - exp_a),
-       rating_b + K * (1 - outcome_a - exp_b)].map(&:round)
+      [rating_a + @k * (outcome - exp_a),
+       rating_b + @k * (1 - outcome - exp_b)].map(&:round)
+    end
+
+    class Player
+      include Comparable
+
+      def self.init_pool(count, elo)
+        Array.new(count) { Player.new(elo) }
+      end
+
+      attr_accessor :rating, :elo
+
+      def initialize(elo)
+        raise(ArgumentError, elo.inspect) unless elo.is_a? Elo
+        @elo = elo
+        @rating = @elo.initial
+      end
+
+      def <=>(opponent)
+        @rating <=> opponent.rating
+      end
+
+      def update(opponent, outcome)
+        @rating, opponent.rating = @elo.update(@rating,
+                                               opponent.rating,
+                                               outcome)
+      end
     end
   end
 end
