1ace9e8815c9b53489668b70332f1605a23842a7
rwh <rwh@arch-intel>
2025-07-22 12:45:49 -0400

    revise test/bench/unix_path.rb

--- test/bench/unix_path.rb
+++ test/bench/unix_path.rb
@@ -3,10 +3,12 @@ require 'compsci/unix_path'
 
 include CompSci
 
-AF = %w[a b c d e f]
+AF = ('a'..'f')
 AF_MU = AF.map { |s| MutablePath.parse(s) }
-AF_IMMU = AF.map { |s| ImmutablePath.parse(s) }
+AF_IM = AF.map { |s| ImmutablePath.parse(s) }
 
+# compare UnixPath / String to UnixPath / UnixPath
+# compare MutablePath to ImmutablePath
 Benchmark.ips do |b|
   b.config(warmup: 0.2, time: 1)
 
@@ -20,12 +22,33 @@ Benchmark.ips do |b|
 
   b.report("ImmutablePath / UnixPath") do
     path = ImmutablePath.new
-    AF_IMMU.each { |p| path = path / p }
+    AF_IM.each { |p| path = path / p }
   end
 
   b.report("MutablePath / UnixPath") do
     path = MutablePath.new
     AF_MU.each { |p| path / p }
+  end
+
+  b.compare!
+end
+
+AZ = ('a'..'z')
+AZ_MU = AZ.map { |s| MutablePath.parse(s) }
+AZ_IM = AZ.map { |s| ImmutablePath.parse(s) }
+
+# now just doing UnixPath / UnixPath but with more chained slashes
+Benchmark.ips do |b|
+  b.config(warmup: 0.2, time: 1)
+
+  b.report("ImmutablePath a-z") do
+    path = ImmutablePath.new
+    AZ_IM.each { |p| path = path / p }
+  end
+
+  b.report("MutablePath a-z") do
+    path = MutablePath.new
+    AZ_MU.each { |p| path / p }
   end
 
   b.compare!
